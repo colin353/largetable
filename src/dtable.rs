fc35297f30c44d2cde06d1ed786cd9011c8e9566
Colin Merkel <colin.merkel@podkarma.com>
2017-01-30 16:28:26 -0500

    recall from DTables

--- src/dtable.rs
+++ src/dtable.rs
@@ -1,135 +1,40 @@
-#[derive(Debug)]
-pub struct DEntry {
-    timestamp: i64,
-    value: Vec<u8>,
-}
+use generated::dtable::DEntry as DEntry;
+use generated::dtable::DColumn as DColumn;
+use generated::dtable::DRow as DRow;
 
-#[derive(Debug)]
-pub struct DColumn {
-    key: String,
-    offset: i64,
-    entries: Vec<DEntry>,
-}
-
-#[derive(Debug)]
-pub struct DRow {
-    key: String,
-    offset: i64,
-    columns: Vec<DColumn>,
-}
-
-#[derive(Debug)]
-pub struct DTable {
-    filename: String,
-    rows: Vec<DRow>
-}
-
-impl DTable {
-    pub fn from_vec(v: Vec<String>) -> DTable {
-        DTable{
-            filename: String::from("1.table"),
-            rows: v.into_iter()
-                    .map(|s| DRow{
-                        key: s,
-                        offset: 0,
-                        columns: vec![]
-                    })
-                    .collect::<Vec<DRow>>()
+impl DColumn {
+    pub fn get_value(&self) -> Option<&[u8]> {
+        let entries = self.get_entries();
+        match entries.len() {
+            0 => None,
+            n => Some(entries[n-1].get_value())
         }
-    }
-
-    pub fn get_value(&self, row: String, column: String, timestamp: i64) -> Option<&[u8]> {
-        return match self.get_row(row) {
-            Some(r) => r.get_value(column, timestamp),
-            None    => None
-        }
-    }
-
-    #[allow(dead_code)]
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let output = vec![];
-
-        return output;
-    }
-
-    pub fn get_row(&self, key: String) -> Option<&DRow> {
-        let mut i = 1;
-        let length = self.rows.len();
-        let mut offset = length >> 1;
-        while (length >> i) > 0 {
-            i += 1;
-            if key == self.rows[offset].key {
-                return Some(&self.rows[offset]);
-            }
-            else if key > self.rows[offset].key {
-                offset += length >> i;
-            }
-            else {
-                offset -= length >> i;
-            }
-        }
-        return None;
     }
 }
 
 impl DRow {
-    pub fn get_column(&self, key: String) -> Option<&DColumn> {
-        let mut i = 1;
-        let length = self.columns.len();
-        let mut offset = length >> 1;
-        while (length >> i) > 0 {
-            i += 1;
-            if key == self.columns[offset].key {
-                return Some(&self.columns[offset])
-            }
-            else if key > self.columns[offset].key {
-                offset += length >> i;
-            }
-            else {
-                offset -= length >> i;
+    pub fn get_column(&self, key: &str) -> Option<&DColumn> {
+        println!("searching for column: {}", key);
+        let keys = self.get_keys();
+        let mut l: i32 = 0;
+        let mut r: i32 = keys.len() as i32 - 1;
+
+        while l <= r {
+            let index = (l + r) >> 1;
+            println!("index = {}", index);
+            match &keys[index as usize] {
+                k if key == k => return Some(&self.get_columns()[index as usize]),
+                k if key > k  => l = index + 1,
+                _             => r = index - 1
             }
         }
         return None;
     }
 
-    pub fn get_value(&self, column: String, timestamp: i64) -> Option<&[u8]> {
-        return match self.get_column(column) {
-            Some(c) => c.get_value(timestamp),
-            None    => None
-        }
-    }
-}
-
-impl DColumn {
-    pub fn get_value(&self, timestamp: i64) -> Option<&[u8]> {
-        return None
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn can_create_and_probe() {
-        let d = DTable::from_vec(vec![
-            String::from("apples"),
-            String::from("bananas"),
-            String::from("oranges"),
-            String::from("yams")
-        ]);
-
-        let yams = d.get_row(String::from("yams"));
-        assert_eq!(yams.unwrap().offset, 0);
-
-        match d.get_row(String::from("clams")) {
-            Some(_) => panic!("Shouldn't get a value."),
-            None    => ()
-        }
-
-        match d.get_value(String::from("apples"), String::from("nutrition"), 10) {
-            Some(_) => panic!("Shouldn't get a value."),
-            None    => ()
-        }
+    pub fn get_value(&self, key: &str) -> Option<&[u8]> {
+        return match self.get_column(key) {
+            Some(col) => col.get_value(),
+            None      => None
+        };
     }
 }
--- src/mtable.rs
+++ src/mtable.rs
@@ -7,6 +7,7 @@
 use std::collections::BTreeMap;
 use std::iter::FromIterator;
 use std::io;
+use std::str::FromStr;
 
 use std::fs::File;
 
@@ -17,22 +18,23 @@ use protobuf::RepeatedField;
 use generated::dtable::DEntry as DEntry;
 use generated::dtable::DColumn as DColumn;
 use generated::dtable::DRow as DRow;
+use dtable;
 use byteorder::{LittleEndian, ReadBytesExt};
 
-struct MUpdate {
+struct MUpdate<'a> {
     value: Vec<u8>,
-    key: String
+    key: &'a str
 }
 
-struct MRow {
-    columns: BTreeMap<String, DColumn>
+struct MRow<'a> {
+    columns: BTreeMap<&'a str, DColumn>
 }
 
-struct MTable {
-    rows: BTreeMap<String, MRow>
+struct MTable<'a> {
+    rows: BTreeMap<&'a str, MRow<'a>>
 }
 
-impl MRow {
+impl <'a> MRow<'a> {
     fn write_to_writer(&self, w: &mut io::Write) -> Result<u64, io::Error> {
         // First, construct a DRow using this MRow, then
         // write out that DRow using write_to_writer.
@@ -45,32 +47,32 @@ impl MRow {
         // to be read into memory in a single read, then binary search
         // is used to find the columns to probe using the lookup table.
         drow.set_keys(protobuf::RepeatedField::from_iter(
-            self.columns.iter().map(|(key, value)| key.clone())
+            self.columns.iter().map(|(key, value)| String::from_str(key).unwrap())
         ));
 
         drow.write_to_writer(w)?;
 
-        return Ok(0);
+        return Ok(drow.get_cached_size() as u64);
     }
 }
 
-impl MTable {
-    fn new() -> MTable {
+impl <'a> MTable<'a> {
+    fn new() -> MTable<'a> {
         return MTable{rows: BTreeMap::new()};
     }
 
-    pub fn update(&mut self, row: String, updates: Vec<MUpdate>) -> Result<(), io::Error>{
+    pub fn update(&mut self, row: &'a str, updates: Vec<MUpdate<'a>>) -> Result<(), io::Error>{
         return match self.rows.get_mut(&row) {
             None    => Err(io::Error::new(io::ErrorKind::NotFound, "No such row.")),
             Some(r) => Ok(r.update(updates))
         };
     }
 
-    pub fn get_row(&self, row: String) -> Option<&MRow> {
+    pub fn get_row(&self, row: &'a str) -> Option<&MRow> {
         self.rows.get(&row)
     }
 
-    pub fn insert(&mut self, row: String, updates: Vec<MUpdate>) {
+    pub fn insert(&mut self, row: &'a str, updates: Vec<MUpdate<'a>>) {
         let r = MRow{
             columns: updates.into_iter().map(|update| {
                 let mut e = DEntry::new();
@@ -87,7 +89,7 @@ impl MTable {
         self.rows.insert(row, r);
     }
 
-    pub fn select(&self, row: String, column: String) -> Option<&[u8]> {
+    pub fn select(&'a self, row: &'a str, column: &'a str) -> Option<&[u8]> {
         match self.rows.get(&row) {
             Some(r) => {
                 match r.columns.get(&column) {
@@ -103,8 +105,8 @@ impl MTable {
     }
 }
 
-impl MRow {
-    fn update(&mut self, updates: Vec<MUpdate>) {
+impl <'a> MRow<'a> {
+    fn update(&mut self, updates: Vec<MUpdate<'a>>) {
         for update in updates {
             match self.columns.get_mut(&update.key) {
                 Some(col) => {
@@ -132,49 +134,66 @@ impl MRow {
 #[cfg(test)]
 mod tests {
     use std;
+    use std::io::Read;
     use protobuf;
+    use protobuf::Message;
     use generated::dtable::DRow as DRow;
 
     #[test]
-    fn can_insert() {
+    fn can_insert_and_retrieve() {
         let mut m = super::MTable::new();
 
-        m.insert(String::from("colin"), vec![super::MUpdate{
-            key: String::from("marfans"),
+        m.insert("colin", vec![super::MUpdate{
+            key: "marfans",
             value: vec![1]
         }]);
 
-        m.update(String::from("colin"), vec![super::MUpdate{
-            key: String::from("friends"),
+        m.update("colin", vec![super::MUpdate{
+            key: "friends",
             value: vec![12,23]
         }]).unwrap();
 
         // Limited scope, so we free the borrowed terms.
         {
-            let has_disease = m.select(String::from("colin"), String::from("marfans")).unwrap();
+            let has_disease = m.select("colin", "marfans").unwrap();
             assert_eq!(has_disease[0], 1);
         }
 
-        match m.select(String::from("colin"), String::from("marfonzo")) {
+        match m.select("colin", "marfonzo") {
             Some(_) => panic!("Shouldn't have disease"),
             None    => ()
         }
 
-        m.update(String::from("colin"), vec![super::MUpdate{
-            key: String::from("marfans"),
+        m.update("colin", vec![super::MUpdate{
+            key: "marfans",
             value: vec![0]
         }]).unwrap();
 
+        m.update("colin", vec![super::MUpdate{
+            key: "christmas",
+            value: vec![44]
+        }]).unwrap();
+
+        m.update("colin", vec![super::MUpdate{
+            key: "mormons",
+            value: vec![0]
+        }]).unwrap();
+
+        m.update("colin", vec![super::MUpdate{
+            key: "jesus",
+            value: vec![66]
+        }]).unwrap();
+
         {
-            let has_disease = m.select(String::from("colin"), String::from("marfans")).unwrap();
+            let has_disease = m.select("colin", "marfans").unwrap();
             assert_eq!(has_disease[0], 0);
         }
 
         let mut f = std::fs::File::create("./data/state.bin").unwrap();
 
-        match m.get_row(String::from("colin")) {
+        match m.get_row("colin") {
             Some(r) => {
-                r.write_to_writer(&mut f);
+                let length = r.write_to_writer(&mut f).unwrap();
                 ()
             },
             None    => panic!("Should be able to get + write row."),
@@ -183,6 +202,14 @@ mod tests {
         let mut g = std::fs::File::open("./data/state.bin").unwrap();
 
         let row = protobuf::parse_from_reader::<DRow>(&mut g).unwrap();
-        assert_eq!(["friends", "marfans"], row.get_keys());
+        assert_eq!(["christmas", "friends", "jesus", "marfans", "mormons"], row.get_keys());
+
+        row.get_value("marfans").unwrap();
+        row.get_value("mormons").unwrap();
+        row.get_value("friends").unwrap();
+        row.get_value("jesus").unwrap();
+        row.get_value("christmas").unwrap();
+        assert_eq!(row.get_value("clapton"), None);
+        assert_eq!(row.get_value("jesus").unwrap(), &[66]);
     }
 }
